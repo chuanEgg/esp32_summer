97c1ee8a9c8f420e05d9f00f9bd2ac8250b03f88
chuan <b12901049@ntu.edu.tw>
2025-07-03 00:55:15 +0800

    minor errors

--- ParellelMatrixMul/main/main.c
+++ ParellelMatrixMul/main/main.c
@@ -17,7 +17,6 @@ int current_row = 0;
 static SemaphoreHandle_t row_mutex;
 static SemaphoreHandle_t sum_mutex;
 static SemaphoreHandle_t done_count_mul;
-static SemaphoreHandle_t done_count_sum;
 
 void multiply_task(void *arg)
 {
@@ -43,42 +42,15 @@ void multiply_task(void *arg)
         result += M1[row][k] * M2[k][col];
       }
       M3[row][col] += result;
+      xSemaphoreTake(sum_mutex, portMAX_DELAY);
+      sum += result;
+      xSemaphoreGive(sum_mutex);
     }
     end_time = esp_timer_get_time();
     duration = end_time - start_time;
     ESP_LOGI(TAG, "core%d: row%d in %lld", core_id, row, duration);
   }
   xSemaphoreGive(done_count_mul);
-  vTaskDelete(NULL);
-}
-
-void sum_task(void *arg)
-{
-  int core_id = esp_cpu_get_core_id();
-  int64_t start_time, end_time, duration = 0;
-  int row;
-  while (true)
-  {
-    start_time = esp_timer_get_time();
-    xSemaphoreTake(row_mutex, portMAX_DELAY);
-    if (current_row >= MATRIX_SIZE)
-    {
-      xSemaphoreGive(row_mutex);
-      break;
-    }
-    row = current_row++;
-    xSemaphoreGive(row_mutex);
-    xSemaphoreTake(sum_mutex, portMAX_DELAY);
-    for (int col = 0; col < MATRIX_SIZE; col++)
-    {
-      sum += M3[row][col];
-    }
-    xSemaphoreGive(sum_mutex);
-    end_time = esp_timer_get_time();
-    duration = end_time - start_time;
-    ESP_LOGI(TAG, "core%d: row%d in %lld", core_id, row, duration);
-  }
-  xSemaphoreGive(done_count_sum);
   vTaskDelete(NULL);
 }
 
@@ -98,7 +70,6 @@ void app_main(void)
   row_mutex = xSemaphoreCreateMutex();
   sum_mutex = xSemaphoreCreateMutex();
   done_count_mul = xSemaphoreCreateCounting(2, 0);
-  done_count_sum = xSemaphoreCreateCounting(2, 0);
 
   xTaskCreatePinnedToCore(multiply_task, "multiply_task_A", 4096, NULL, 5, NULL, 0);
   xTaskCreatePinnedToCore(multiply_task, "multiply_task_B", 4096, NULL, 5, NULL, 1);
@@ -114,14 +85,5 @@ void app_main(void)
       ESP_LOGI(TAG, "M3[%d][%d] = %d", i, j, M3[i][j]);
     }
   }
-  current_row = 0;
-  start_time = esp_timer_get_time();
-  xTaskCreatePinnedToCore(sum_task, "sum_task_A", 4096, NULL, 5, NULL, 0);
-  xTaskCreatePinnedToCore(sum_task, "sum_task_B", 4096, NULL, 5, NULL, 1);
-
-  xSemaphoreTake(done_count_sum, portMAX_DELAY);
-  xSemaphoreTake(done_count_sum, portMAX_DELAY);
-  end_time = esp_timer_get_time();
-  ESP_LOGI(TAG, "Sum done in %lld", end_time - start_time);
   ESP_LOGI(TAG, "Total sum = %d", sum);
 }
